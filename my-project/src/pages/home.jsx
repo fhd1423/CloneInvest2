47c4d11dd2de8cac7f42cde7ccfc77a1955cda5d
fhd1423 <rohithvenkatesan142@gmail.com>
2023-03-25 20:28:29 -0400

    navbar finished

--- my-project/src/pages/home.jsx
+++ my-project/src/pages/home.jsx
@@ -1,24 +1,27 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import portfolio from 'src/public/portfolio.jpg'
+import { useRouter } from 'next/router'
+
 
 export default function Home() {
+  const router = useRouter()
   return (
     <div>
 
-        <nav class="bg-gray-800">
-            <div class="w-full h-16 flex flex-row items-center px-5 text-slate-100 justify-between">
-                <ul class="flex">
-                    <li class="mx-7">Home</li>
-                    <li class="mx-7">Create Event</li>
-                </ul>
-                <div class="">profile</div>
-            </div>
-        </nav>
-
-        <div>
-
+      <nav class="bg-gray-800">
+        <div class="w-full h-16 flex flex-row items-center px-5 text-slate-100 justify-between">
+          <ul class="flex">
+            <button onClick={() => router.push('/home')} class="mx-7 rounded-md border-2 border-white p-2 hover:bg-white hover:text-black">Home</button>
+            <button onClick={() => router.push('/createEvent')} class="mx-7 rounded-md border-2 border-white p-2 hover:bg-white hover:text-black">Create Event</button>
+          </ul>
+          <button onClick={() => router.push('/profile')} class="rounded-md border-2 border-white p-2 hover:bg-white hover:text-black">Profile</button>
         </div>
+      </nav>
+
+      <div>
+
+      </div>
 
     </div>
   )
